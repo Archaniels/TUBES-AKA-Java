42f05eddb05f1a07b854ba565a420bb668123b63
Daniyal Arshaq S. <89289650+Archaniels@users.noreply.github.com>
2024-12-11 18:20:52 +0700

    Update GerakanKuda.java

--- src/main/java/com/method/tubesaka/GerakanKuda.java
+++ src/main/java/com/method/tubesaka/GerakanKuda.java
@@ -5,18 +5,26 @@ public class GerakanKuda {
     static int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
     
     public int HitungLangkahBFS_Iteratif(int n, int x1, int y1, int x2, int y2, int k) {
-        int[][][] dp = new int[n][n][k + 1];
-        dp[x1][y1][0] = 1;
+        /*
+        n = Ukuran papan catur (N x N)
+        (x1, y1) = Koordinat awal kuda
+        (x2, y2) = Koordinat tujuan kuda
+        k = jumlah step yang diinginkan
+        */
+        
+        // array 3D
+        int[][][] status = new int[n][n][k + 1];
+        status[x1][y1][0] = 1;
 
         for (int steps = 1; steps <= k; steps++) {
             for (int x = 0; x < n; x++) {
                 for (int y = 0; y < n; y++) {
-                    if (dp[x][y][steps - 1] > 0) {
+                    if (status[x][y][steps - 1] > 0) {
                         for (int i = 0; i < 8; i++) {
                             int nx = x + dx[i];
-                            int ny = y + dy[i];
+                            int     ny = y + dy[i];
                             if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                                dp[nx][ny][steps] += dp[x][y][steps - 1];
+                                status[nx][ny][steps] += status[x][y][steps - 1];
                             }
                         }
                     }
@@ -25,7 +33,7 @@ public class GerakanKuda {
         }
         int hasil = 0;
         for (int steps = 0; steps <= k; steps++) {
-            hasil += dp[x2][y2][steps];
+            hasil += status[x2][y2][steps];
         }
         return hasil;
     }
